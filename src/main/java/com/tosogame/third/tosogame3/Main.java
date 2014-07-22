d29dafbd4433aee8b223ca32fb9277d2a0dde37e
peko <peko@192.168.0.2>
2014-07-22 22:46:23 +0900

    maven

---
+++ src/main/java/com/tosogame/third/tosogame3/Main.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tosogame.third.tosogame3;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author ja731j
+ */
+public class Main extends JavaPlugin{
+        @Override
+    public void onEnable() {
+        getLogger().info("The TosoGame3 plugin has been loaded");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("The TosoGame3 plugin has been unloaded");
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(sender instanceof Player){
+            Player player = (Player)sender;
+            player.sendMessage("Hello, "+player.getName()+"!");
+        }else{
+            sender.sendMessage("This command can only be run by players!");
+        }
+        return true;
+    }
+}

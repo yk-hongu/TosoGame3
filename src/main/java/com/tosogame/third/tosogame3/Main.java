313742e581285feb26de9347c1be3da51dc0e49d
pekomiya <>
2014-07-22 22:47:53 +0900

    maven

--- src/main/java/com/tosogame/third/tosogame3/Main.java
+++ src/main/java/com/tosogame/third/tosogame3/Main.java
@@ -16,12 +16,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin{
         @Override
     public void onEnable() {
-        getLogger().info("The TosoGame3 plugin has been loaded");
+        getLogger().info("The TosoGame3 plugin has been loaded!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("The TosoGame3 plugin has been unloaded");
+        getLogger().info("The TosoGame3 plugin has been unloaded!");
     }
     
     @Override
